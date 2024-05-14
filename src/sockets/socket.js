759af530c62db2c0ff9fc0e0718f6bd467d3732c
Henrique De Antoni <147777120+henriquedeantoni@users.noreply.github.com>
2024-05-14 10:21:52 -0300

    Update socket.js

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -1,6 +1,6 @@
 import socketIOClient from "socket.io-client";
 
-const ENDPOINT = "http://localhost:3001"; // URL do seu servidor Socket.IO
+const ENDPOINT = "http://localhost:3001"; // URL do servidor Socket.IO
 
 const socket = socketIOClient(ENDPOINT);
 
